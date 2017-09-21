082470eb2db3c085c11e40a984f4df737e358882
Zeral <zeralzhang@gmail.com>
2017-09-21 23:28:34 +0800

    配置liqui base

--- src/main/java/com/tongwii/config/DatabaseConfiguration.java
+++ src/main/java/com/tongwii/config/DatabaseConfiguration.java
@@ -1,16 +1,15 @@
 package com.tongwii.config;
 
-<<<<<<< HEAD
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.task.TaskExecutor;
 import com.tongwii.constant.TongWiiConstants;
 import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
