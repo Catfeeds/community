035c55435c96eeea565c521658f9ea0bc104397e
zeral <zeralzhang@gmail.com>
2017-09-28 09:52:06 +0800

    提交room文件

--- src/main/java/com/tongwii/domain/RoomEntity.java
+++ src/main/java/com/tongwii/domain/RoomEntity.java
@@ -1,6 +1,6 @@
 package com.tongwii.domain;
 
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -14,118 +14,38 @@ import java.util.Collection;
  * @date: 2017/7/13
  */
 @Entity
-@EqualsAndHashCode
+@Data
 @Table(name = "room", schema = "cloud_community", catalog = "")
 public class RoomEntity implements Serializable {
-    private String id;
-    private String roomCode;
-    private Double area;
-    private String huXing;
-    private String clientCode;
-    private String ownerId;
-    private String unitId;
-    private UserEntity userByOwnerId;
-    private FloorEntity floorByUnitId;
-    private Collection<UserRoomEntity> userRoomsById;
-
     @Id
     @GeneratedValue(generator = "uuidGenerator")
     @GenericGenerator(name = "uuidGenerator", strategy = "uuid2")
     @Column(name = "id", unique = true, nullable = false, length = 36)
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
+    private String id;
     @Basic
     @Column(name = "room_code")
-    public String getRoomCode() {
-        return roomCode;
-    }
-
-    public void setRoomCode(String roomCode) {
-        this.roomCode = roomCode;
-    }
-
+    private String roomCode;
     @Basic
     @Column(name = "area")
-    public Double getArea() {
-        return area;
-    }
-
-    public void setArea(Double area) {
-        this.area = area;
-    }
-
+    private Double area;
     @Basic
     @Column(name = "hu_xing")
-    public String getHuXing() {
-        return huXing;
-    }
-
-    public void setHuXing(String huXing) {
-        this.huXing = huXing;
-    }
-
+    private String huXing;
     @Basic
     @Column(name = "client_code")
-    public String getClientCode() {
-        return clientCode;
-    }
-
-    public void setClientCode(String clientCode) {
-        this.clientCode = clientCode;
-    }
-
+    private String clientCode;
     @Basic
     @Column(name = "owner_id")
-    public String getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(String ownerId) {
-        this.ownerId = ownerId;
-    }
-
+    private String ownerId;
     @Basic
     @Column(name = "unit_id")
-    public String getUnitId() {
-        return unitId;
-    }
-
-    public void setUnitId(String unitId) {
-        this.unitId = unitId;
-    }
-
+    private String unitId;
     @ManyToOne
     @JoinColumn(name = "owner_id", referencedColumnName = "id", insertable = false, updatable = false)
-    public UserEntity getUserByOwnerId() {
-        return userByOwnerId;
-    }
-
-    public void setUserByOwnerId(UserEntity userByOwnerId) {
-        this.userByOwnerId = userByOwnerId;
-    }
-
+    private UserEntity userByOwnerId;
     @ManyToOne
     @JoinColumn(name = "unit_id", referencedColumnName = "id", insertable = false, updatable = false)
-    public FloorEntity getFloorByUnitId() {
-        return floorByUnitId;
-    }
-
-    public void setFloorByUnitId(FloorEntity floorByUnitId) {
-        this.floorByUnitId = floorByUnitId;
-    }
-
+    private FloorEntity floorByUnitId;
     @OneToMany(mappedBy = "roomByRoomId")
-    public Collection<UserRoomEntity> getUserRoomsById() {
-        return userRoomsById;
-    }
-
-    public void setUserRoomsById(Collection<UserRoomEntity> userRoomsById) {
-        this.userRoomsById = userRoomsById;
-    }
+    private Collection<UserRoomEntity> userRoomsById;
 }
