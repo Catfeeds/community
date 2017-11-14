aaa0be063859f1a4f1def637f0b4609223a3c04e
zeral <zeralzhang@gmail.com>
2017-11-14 19:35:17 +0800

    去掉controller内try.catch,使用全局异常处理

--- src/main/java/com/tongwii/controller/UserContactController.java
+++ src/main/java/com/tongwii/controller/UserContactController.java
@@ -28,7 +28,7 @@ public class UserContactController {
      * @return result
      * */
     @PostMapping(value = "/addUserContacts")
-    public ResponseEntity addUserContacts(@RequestBody UserContact userContact){
+    public ResponseEntity addUserContacts(@RequestBody UserContact userContact) {
         String userId = SecurityUtils.getCurrentUserId();
 
         List<UserContact> userContactEntities = userContactService.findByUserId(userId);
@@ -42,7 +42,6 @@ public class UserContactController {
             return ResponseEntity.badRequest().body("该联系人信息已存在!");
         }
         userContact.setUserId(userId);
-//        userContact.setFriendId(friend.getId());
         userContactService.addUserContact(userContact);
         return ResponseEntity.ok(userContact);
     }
@@ -52,49 +51,45 @@ public class UserContactController {
      *
      * @return result tong wii result
      */
-    @GetMapping()
-    public ResponseEntity getContactsByUserId(){
-        try {
-            String userId = SecurityUtils.getCurrentUserId();
-            List<UserContact> userContactList = userContactService.findByUserId(userId);
-            if(CollectionUtils.isEmpty(userContactList)){
-                return ResponseEntity.badRequest().body("此用户没有联系人!");
-            }
-            Map<String, List> contactMap = new TreeMap<>();
-            for(UserContact userContact : userContactList){
-                User friend = userContact.getUserByFriendId();
-                String pinYin = userContact.getDes();
-                if(StringUtils.isEmpty(pinYin)) {
-                    pinYin = friend.getNickName();
-                }
-                String sortString = PinYinUtil.converterToFirstChar(pinYin);
-                Map<String, Object> object = new HashMap<>();
-                object.put("contactAccount", friend.getAccount());
-                object.put("contactName", friend.getName());
-                object.put("contactDesc", userContact.getDes());
-                object.put("contactClientId", friend.getClientId());
-                object.put("contactPhone", friend.getPhone());
-                if(StringUtils.isNotEmpty(friend.getAvatarFileId())) {
-                    object.put("contactPhoto", friend.getFileByAvatarFileId().getFilePath());
-                }
-                object.put("contactId", userContact.getId());
-                if (sortString.matches("[A-Z]")) {
-                    if (!contactMap.containsKey(sortString)) {
-                        contactMap.put(sortString, new ArrayList());
-                    }
-                    contactMap.get(sortString).add(object);
-                } else {
-                    if(!contactMap.containsKey(UserContact.UNKNOWN_NAME)) {
-                        // 存放不是A-Z字母的联系人名称
-                        contactMap.put(UserContact.UNKNOWN_NAME, new ArrayList());
-                    }
-                    contactMap.get(UserContact.UNKNOWN_NAME).add(object);
-                }
-            }
-            return ResponseEntity.ok(contactMap);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("联系人列表获取失败!");
+    @GetMapping
+    public ResponseEntity getContactsByUserId() {
+        String userId = SecurityUtils.getCurrentUserId();
+        List<UserContact> userContactList = userContactService.findByUserId(userId);
+        if (CollectionUtils.isEmpty(userContactList)) {
+            return ResponseEntity.badRequest().body("此用户没有联系人!");
         }
+        Map<String, List<Map>> contactMap = new TreeMap<>();
+        for (UserContact userContact : userContactList) {
+            User friend = userContact.getUserByFriendId();
+            String pinYin = userContact.getDes();
+            if (StringUtils.isEmpty(pinYin)) {
+                pinYin = friend.getNickName();
+            }
+            String sortString = PinYinUtil.converterToFirstChar(pinYin);
+            Map<String, Object> object = new HashMap<>();
+            object.put("contactAccount", friend.getAccount());
+            object.put("contactName", friend.getName());
+            object.put("contactDesc", userContact.getDes());
+            object.put("contactClientId", friend.getClientId());
+            object.put("contactPhone", friend.getPhone());
+            if (StringUtils.isNotEmpty(friend.getAvatarFileId())) {
+                object.put("contactPhoto", friend.getFileByAvatarFileId().getFilePath());
+            }
+            object.put("contactId", userContact.getId());
+            if (sortString.matches("[A-Z]")) {
+                if (!contactMap.containsKey(sortString)) {
+                    contactMap.put(sortString, new ArrayList<>());
+                }
+                contactMap.get(sortString).add(object);
+            } else {
+                if (!contactMap.containsKey(UserContact.UNKNOWN_NAME)) {
+                    // 存放不是A-Z字母的联系人名称
+                    contactMap.put(UserContact.UNKNOWN_NAME, new ArrayList<>());
+                }
+                contactMap.get(UserContact.UNKNOWN_NAME).add(object);
+            }
+        }
+        return ResponseEntity.ok(contactMap);
     }
 
     /**
@@ -103,12 +98,8 @@ public class UserContactController {
      * @return result
      * */
     @PutMapping(value="/delectContact/{contactId}")
-    public ResponseEntity delectContact(@PathVariable String contactId){
-        try{
-            userContactService.delete(contactId);
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body("该联系人信息不存在!");
-        }
+    public ResponseEntity delectContact(@PathVariable String contactId) {
+        userContactService.delete(contactId);
         return ResponseEntity.ok("联系人信息删除成功!");
     }
 }
