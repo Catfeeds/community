f6a750bc8937c960ad4f40c11807a189d4e60441
zeral <zeralzhang@gmail.com>
2017-12-05 17:24:48 +0800

    添加后台webSocket配置

---
+++ src/main/java/com/tongwii/config/WebSocketSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.tongwii.config;
+
+import com.tongwii.constant.AuthoritiesConstants;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+            .nullDestMatcher().authenticated().simpDestMatchers("/topic/tracker")
+            .hasAuthority(AuthoritiesConstants.ADMIN).simpDestMatchers("/topic/message")
+            .hasAuthority(AuthoritiesConstants.ADMIN)
+            // matches any destination that starts with /topic/
+            // (i.e. cannot send messages directly to /topic/)
+            // (i.e. cannot subscribe to /topic/messages/* to get messages sent to
+            // /topic/messages-user<id>)
+            .simpDestMatchers("/topic/**").authenticated()
+            // matches any destination that starts with /chat/private and /chat/*
+            .simpDestMatchers("/chat/private*").hasAuthority(AuthoritiesConstants.USER)
+            .simpDestMatchers("/chat/**").permitAll()
+            // message types other than MESSAGE and SUBSCRIBE
+            .simpTypeMatchers(SimpMessageType.MESSAGE, SimpMessageType.SUBSCRIBE).denyAll()
+            // catch all
+            .anyMessage().denyAll();
+    }
+
+    /**
+     * Disables CSRF for WebSockets.
+     */
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
